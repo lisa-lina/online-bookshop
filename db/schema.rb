a2d850addc676b27531fdbaaeadedc153efef9f4
Lisa Lee <zaidaleelina@yahoo.com>
2015-10-21 01:59:20 -0400

    create scaffold cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016073321) do
+ActiveRecord::Schema.define(version: 20151021055808) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"

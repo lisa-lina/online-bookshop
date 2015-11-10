5673504aa923152498ad79700b1515891ea6cadf
Lisa <nali@tibco-support.com>
2015-11-10 16:52:59 +0800

    fix ajax test

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -32,7 +32,7 @@ class LineItemsControllerTest < ActionController::TestCase
     assert_response :success
 
     assert_select_jquery :html, '#cart' do
-      assert_select 'tr#current_item td', /Porgramming Ruby 1.9/
+      assert_select 'tr#current_item td', /Programming Ruby 1.9/
     end
   end
 

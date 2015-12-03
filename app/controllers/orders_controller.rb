bcd5da16d91aaea4cbcd84c360386ba85669f611
Lisa <nali@tibco-support.com>
2015-12-03 16:20:05 +0800

    checkout

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,7 +14,16 @@ class OrdersController < ApplicationController
 
   # GET /orders/new
   def new
+    @cart = current_cart
+    if @cart.line_items.empty?
+      redirect_to store_url, notice: "Your cart is empty"
+      return
+    end
     @order = Order.new
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json {render json: @order}
+    end
   end
 
   # GET /orders/1/edit

48f9856b7d021180e6abe2c9107dcce5bd169bc5
Lisa <nali@tibco-support.com>
2015-12-14 14:53:39 -0800

    checkout - capture an order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -33,14 +33,17 @@ class OrdersController < ApplicationController
   # POST /orders
   # POST /orders.json
   def create
-    @order = Order.new(order_params)
-
+    @order = Order.new(params[:order])
+    @order.add_line_items_from_cart(current_cart)
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
-        format.json { render :show, status: :created, location: @order }
+        Cart.destroy(session[:cart_id])
+        session[:cart_id] = nil
+        format.html { redirect_to store_url, notice: 'Thank you for your order.' }
+        format.json { render json: @order, status: :created, location: @order }
       else
-        format.html { render :new }
+        @cart = current_cart
+        format.html { render action: "new" }
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end

ada28a20b3dd9ae7be301793a59c3a6c36e6f60a
Lisa <nali@tibco-support.com>
2015-12-17 17:13:57 -0800

    fix cannot list orders issue - 2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,12 +5,11 @@ class OrdersController < ApplicationController
   # GET /orders.json
   def index
     # @orders = Order.all
-    @orders = Order.paginate page: params[:page], order: 'created_at desc',
-    per_page: 10
-    # respond_to do |format|
-    #   format.html #index.html.erb
-    #   format.json {render json: @orders}
-    # end
+    @orders = Order('created_at DESC').paginate page: params[:page], per_page: 10
+    respond_to do |format|
+      format.html #index.html.erb
+      format.json {render json: @orders}
+    end
   end
 
   # GET /orders/1

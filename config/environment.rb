bac7eedcef879bff6c783dda51ceb14e43e81d15
Lisa <nali@tibco-support.com>
2016-01-07 14:50:20 -0800

    try to make integration test running

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,20 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      authentication: "plain",
+      user_name: "lisaleelina@gmail.com",
+      password: "Lisa1&Lina1",
+      enable_starttls_auto: true,
+      openssl_verify_mode: 'none'
+  }
+end
+
+
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,18 +29,7 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    authentication: "plain",
-    user_name: "****@gmail.com",
-    password: "****",
-    enable_starttls_auto: true,
-    openssl_verify_mode: 'none'
-  }
+  config.action_mailer.delivery_method = :test
 
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random

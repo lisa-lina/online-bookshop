898372e5df17c117b90ed95550bff6605593681f
Lisa <nali@tibco-support.com>
2016-02-25 16:15:38 -0800

    adding the locale

--- config/routes.rb
+++ config/routes.rb
@@ -7,23 +7,24 @@ Rails.application.routes.draw do
     delete 'logout' => :destroy
   end
 
-  resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  get 'store/index'
+  scope '(:local)' do
+    resources :users
+    resources :orders
+    resources :line_items
+    resources :carts
+    get 'store/index'
 
-  resources :products do
-    get :who_bought, on: :member
+    resources :products do
+      get :who_bought, on: :member
+    end
+    # The priority is based upon order of creation: first created -> highest priority.
+    # See how all your routes lay out with "rake routes".
+
+    # You can have the root of your site routed with "root"
+    # root 'welcome#index'
+
+    root to: 'store#index', as: 'store'
   end
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  root to: 'store#index', as: 'store'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 

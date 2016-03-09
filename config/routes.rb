3e65beb2f589ad638aeb9b192a2af73b04e385e7
Lisa <nali@tibco-support.com>
2016-03-09 13:50:32 -0800

    add i18n in application controlloer

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     resources :orders
     resources :line_items
     resources :carts
-    get 'store/index'
+    # get 'store/index'
 
     resources :products do
       get :who_bought, on: :member
